40326f113d892cfe090c41e5b99ed057e1184c29
Emil Ståhl <emil.stahl@icloud.com>
2021-10-05 01:53:21 +0200

    Added vnc viewer installer

--- install-docker.sh
+++ install-docker.sh
@@ -12,3 +12,7 @@ apt-cache policy docker-ce
 
 sudo apt install docker-ce
 
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
